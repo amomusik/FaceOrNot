4f347fcc7b303c52091b0366d24f3249deebddf7
Sean Heber <sean@iconfactory.com>
2011-10-24 21:15:34 -0500

    Initial Commit

---
+++ FaceOff/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FaceOff
+//
+//  Created by Sean Heber on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FaceOff/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FaceOff
+//
+//  Created by Sean Heber on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
